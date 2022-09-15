62c0a726817f8f805375953ee5b9b7ea4820e5fb
eduardaramalho <eduardacns.ramalho@gmail.com>
2022-09-15 14:59:43 -0300

    add login

---
+++ loginPage/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'paginaLogin';
+  usuario : string = '';
+  senha : string = '';
+
+constructor(private httpClient : HttpClient){
+
+}
+
+public login(){
+  this.httpClient.post('http://localhost:3003/login', {name : this.usuario, password : this.senha}).toPromise().then((response : any)=> {
+    console.log(response);
+  })
+}
+}
